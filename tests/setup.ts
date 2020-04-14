cd14dfc94c76c14d76abb88f181d3b3e45e526b1
neolitec aka Keveun <kevin@xpertsea.com>
2020-04-13 22:14:29 -0400

    Improve pipeline. (#7)
    * Improve pipeline.
    
    * Add pre-commit.
    
    * Fix lint errors.
    
    * Fix pipeline.

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { loadFixtures } from './fixtures-loader';
 import { cleanDatabase } from './db-cleaner';
 
-export default async function() {
+export default async function () {
   // eslint-disable-next-line global-require
   require('dotenv').config({
     path: path.resolve(__dirname, '..', '.env'),
