a0de2d51216832bcf8c01c70ddbfeacbd03e400d
Kevin Manson <kev.manson@gmail.com>
2020-04-18 00:46:48 -0400

    Add trace to debug.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import { initApp } from './app';
 const port = process.env.PORT || 4000;
 
 async function start() {
-  const app = await initApp();
+  try {
+    const app = await initApp();
 
-  app.listen({ port }, () =>
-    console.log(`🚀 Server ready at http://localhost:${port}`),
-  );
+    app.listen({ port }, () =>
+      console.log(`🚀 Server ready at http://localhost:${port}`),
+    );
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 start();
